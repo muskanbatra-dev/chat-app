5552284bcb6e46f7c8736a9ec95e858826efb16a
muskanbatra-dev <muskanbatra64@gmail.com>
2021-12-22 22:27:57 +0530

    update

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import {ChatEngine } from 'react-chat-engine';
+
+import './App.css' ;
+
+const App = ( ) => {
+    return (
+        <ChatEngine
+         height="100vh"
+         projectID="027c749c-40fd-4ac0-a251-06c537bdc62b"
+         userName="Muskan Batra"
+         userSecret="123123"
+
+
+
+
+
+        />
+    );
+
+}
+
+export default App;
