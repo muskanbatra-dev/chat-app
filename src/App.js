06eed25482e72267a6ff1ecd94097c3891d21301
muskanbatra-dev <muskanbatra64@gmail.com>
2021-12-23 16:49:31 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import {ChatEngine } from 'react-chat-engine';
+import {ChatEngine, ChatFeed } from 'react-chat-engine';
 
 import './App.css' ;
 
+import ChatFeed from './components/ChatFeed';
+
 const App = ( ) => {
-    return (
+    return ( 
         <ChatEngine
          height="100vh"
          projectID="027c749c-40fd-4ac0-a251-06c537bdc62b"
          userName="Muskan Batra"
          userSecret="123123"
-         
+         renderChatFeed={(chatAppProps) => <ChatFeed { ...chatAppProps}
+
 
 
  
---
+++ src/components/ChatFeed.jsx
@@ -0,0 +1,30 @@
+import MessageForm from "./MessageForm";
+import MyMessage from "./MyMessage";
+import TheirMessage from "./TheirMessage";
+
+
+ const ChatFeed = (props) => {
+
+    const { chats, activeChat, userName, messages } = props;
+    
+    const chat = chats && chats[activeChat];
+
+    console.log(chat, userName, messages);
+
+    const renderMessages = () => {
+        const keys = Object.keys(messages);
+
+        
+
+renderMessages()
+    }
+
+
+return (
+    <div>
+        ChatFeed
+    </div>
+);
+
+ }
+export default ChatFeed;
