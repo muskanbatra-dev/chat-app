28e6487da3bab6f3cf0c05ec2638cd5738ba0a79
muskanbatra-dev <muskanbatra64@gmail.com>
2022-01-15 14:53:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,13 +5,17 @@ import './App.css' ;
 import ChatFeed from './components/ChatFeed'
 
 const App = () => {
-    return  <ChatEngine
+    return (
+      
+       <ChatEngine
         height="100vh"
         projectID="027c749c-40fd-4ac0-a251-06c537bdc62b"
         userName="Muskan Batra"
         userSecret="123123"
-        renderChatFeed={(chatAppProps) => <ChatFeed { ...chatAppProps}}
-/>
+        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+        onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+        />
+ 
   )    
 }
     
