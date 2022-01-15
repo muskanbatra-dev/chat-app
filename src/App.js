1758be88315a6ee12a45ef4d1df1971b91e4e41f
muskanbatra-dev <muskanbatra64@gmail.com>
2022-01-15 12:40:43 +0530

    fghcg

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import {ChatEngine, ChatFeed } from 'react-chat-engine';
+import {ChatEngine} from 'react-chat-engine';
 
 import './App.css' ;
 
-import ChatFeed from './components/ChatFeed';
+import ChatFeed from './components/ChatFeed'
 
-const App = ( ) => {
-    return ( 
-        <ChatEngine
-         height="100vh"
-         projectID="027c749c-40fd-4ac0-a251-06c537bdc62b"
-         userName="Muskan Batra"
-         userSecret="123123"
-         renderChatFeed={(chatAppProps) => <ChatFeed { ...chatAppProps}
-
-
-
- 
-
-
-        />
-    );
-
+const App = () => {
+    return  <ChatEngine
+        height="100vh"
+        projectID="027c749c-40fd-4ac0-a251-06c537bdc62b"
+        userName="Muskan Batra"
+        userSecret="123123"
+        renderChatFeed={(chatAppProps) => <ChatFeed { ...chatAppProps}}
+/>
+  )    
 }
+    
+
+    
+
+
+
+
 
 export default App;
